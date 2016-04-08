a0e7e56a55f1b9942e32e75a137be93b0485a6bd
Alex Gofman <gofmanalex13@gmail.com>
2016-04-09 01:43:04 +0400

    add Featured component

---
+++ Featured.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React, {
+  StyleSheet,
+  View,
+  Text,
+  Component
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 20,
+    backgroundColor: 'white'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+class Featured extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Featured tab
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.extends = Featured;
