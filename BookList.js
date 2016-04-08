502de1463e49413045e16bb8bbcadb026d3dcd29
Alex Gofman <gofmanalex13@gmail.com>
2016-04-09 02:26:51 +0400

    added components

---
+++ BookList.js
@@ -0,0 +1,22 @@
+'use strict';
+ 
+import React, {
+  StyleSheet,
+  View,
+  Component
+} from 'react-native';
+ 
+const styles = StyleSheet.create({
+ 
+});
+ 
+class BookList extends Component {
+	render() {
+		return (
+    	<View>
+	    </View>             
+    );
+  }
+}
+ 
+module.exports = BookList;
---
+++ SearchBooks.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, {
+  StyleSheet,
+  View,
+  Component
+} from 'react-native';
+ 
+const styles = StyleSheet.create({
+ 
+});
+ 
+class SearchBooks extends Component {
+  render() {
+    return (
+      <View>
+	    </View>             
+    );
+  }
+}
+ 
+module.exports = SearchBooks;
