474d17d632071d58c3edb7918dbe25be722d8f18
Alex Gofman <gofmanalex13@gmail.com>
2016-04-09 10:01:50 +0400

    add fake book data for testing

--- BookList.js
+++ BookList.js
@@ -6,6 +6,15 @@ import React, {
   Component
 } from 'react-native';
  
+const FAKE_BOOK_DATA = [{
+  volumeInfo: {
+    title: 'The Catcher in the Rye',
+    authors: "J. D. Salinger",
+    imageLinks: {
+      thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}
+  }
+}];
+
 const styles = StyleSheet.create({
  
 });
