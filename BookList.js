339f5b8e23dbc09edcd7d64c960c53a9de6c3620
Alex Gofman <gofmanalex13@gmail.com>
2016-04-09 13:27:31 +0400

    implemented styles for booklist

--- BookList.js
+++ BookList.js
@@ -1,7 +1,9 @@
 'use strict';
  
 import React, {
+  Image,
   StyleSheet,
+  Text,
   View,
   Component
 } from 'react-native';
@@ -11,12 +13,35 @@ const FAKE_BOOK_DATA = [{
     title: 'The Catcher in the Rye',
     authors: "J. D. Salinger",
     imageLinks: {
-      thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}
+      thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+    }
   }
 }];
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 10
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+    marginRight: 10
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8
+  },
+  author: {
+    color: '#656565'
+  }
 });
  
 class BookList extends Component {
